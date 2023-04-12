538086222a91d2a9d02d2a4b8cd1d33ab91b74ad
Fabian Martin <fabianmartinanz@gmail.com>
2023-04-11 22:57:00 -0500

    feat: connecting db and pinging

---
+++ config/db.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"database/sql"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
+func Connect() (*gorm.DB, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	//dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+	//	os.Getenv("DB_USER"),
+	//	os.Getenv("DB_PASSWORD"),
+	//	os.Getenv("DB_HOST"),
+	//	os.Getenv("DB_PORT"),
+	//	os.Getenv("DB_DATABASE"))
+
+	//fmt.Println(dns)
+
+	//db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("root:david12@tcp(localhost:3306)/parking?charset=utf8mb4&parseTime=True&loc=Local\n"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return db, nil
+}
+
+func TestDBConnection() error {
+	db, err := Connect()
+	if err != nil {
+		return err
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		return err
+	}
+	defer func(sqlDB *sql.DB) {
+		err := sqlDB.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(sqlDB)
+
+	return sqlDB.Ping()
+}
--- main.go
+++ main.go
@@ -1,25 +1,17 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"github.com/fzbian/parking/config"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
-	"log"
-	"os"
 )
 
 func main() {
-	err := godotenv.Load(".env")
+	err := config.TestDBConnection()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		fmt.Println("Error de conexión a la base de datos:", err)
+		return
 	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE"))
-	_, err = sql.Open("mysql", dsn)
-	if err != nil {
-		fmt.Printf("Cannot connect with database: %s", err.Error())
-	}
-
-	fmt.Println("Connected to database")
+	fmt.Println("Conexión exitosa a la base de datos")
 }
