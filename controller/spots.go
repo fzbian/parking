9873403c8c2b751f7ec086917db546b20cd5129a
Fabian Martin <fabianmartinanz@gmail.com>
2023-04-12 21:17:09 -0500

    feat: ExitVehicle function

--- controller/spots.go
+++ controller/spots.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"errors"
 	"github.com/fzbian/parking/config"
 	"github.com/fzbian/parking/models"
 )
@@ -14,6 +15,34 @@ func InsertVehicle(vehicle *models.Vehicle) error {
 	result := db.Create(vehicle)
 	if result.Error != nil {
 		return result.Error
+	}
+
+	return nil
+}
+
+func ExitVehicle(plateNumber string) error {
+
+	var vehicle *models.Vehicle
+
+	if plateNumber == "" {
+		return errors.New("plate number is required")
+	}
+
+	db, err := config.Connect()
+	if err != nil {
+		panic(err)
+	}
+
+	// Find the vehicle by its plate number
+	result := db.Where("plate_number = ?", plateNumber).First(&vehicle)
+	if result.Error != nil {
+		return errors.New("vehicle not found")
+	}
+
+	// Delete the vehicle
+	result = db.Delete(&vehicle)
+	if result.Error != nil {
+		return errors.New("vehicle can be deleted")
 	}
 
 	return nil
