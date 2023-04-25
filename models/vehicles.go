249f373b9b1836b357b80a796e8ab2a6941a7f22
Fabian Martin <fabianmartinanz@gmail.com>
2023-04-25 18:51:50 +0200

    fix: adding pointer to VehicleSpots

--- models/vehicles.go
+++ models/vehicles.go
@@ -16,6 +16,6 @@ type VehiclesSpots struct {
 	Id        int        `json:"id" db:"id"`
 	VehicleId int        `json:"vehicle_id" db:"vehicle_id"`
 	Spot      int        `json:"spot" db:"spot"`
-	EntryTime time.Time  `json:"entry_time" db:"entry_time"`
+	EntryTime *time.Time  `json:"entry_time" db:"entry_time"`
 	ExitTime  *time.Time `json:"exit_time" db:"exit_time"`
 }
